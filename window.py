d0d3fcd7d633eade2caffe7544fb175978b28630
alex-bazzite-desk <alex@bazzite.fritz.box>
2025-05-23 14:35:10 +0100

    create window class

--- window.py
+++ window.py
@@ -3,21 +3,24 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
-        self.__root.title("My window")
+        self.__root.title = 'title'
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.canvas = Canvas(self.__root, width=width, height=height)
         self.canvas.pack()
-        self.running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.winrun = False
 
     def redraw(self):
-        self.__root.update_idletasks()
         self.__root.update()
+        self.__root.update_idletasks()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.winrun = True
+        while self.winrun:
             self.redraw()
-    
-    def close(self):
-        self.running = False
 
+    def close(self):
+        self.winrun = False
+        
+
+
+    
