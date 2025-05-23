9f5ac514358830998a508ee3b43a84fe85af67bb
alex-bazzite-desk <alex@bazzite.fritz.box>
2025-05-23 15:12:45 +0100

    add draw line

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from window import Window
+from window import Window, Point, Line
+
+lines = [
+    {"point_a": [80, 60], "point_b": [180, 160], "colour": "blue"},
+    {"point_a": [800, 600], "point_b": [0, 0], "colour": "red"},
+    {"point_a": [400, 300], "point_b": [800, 0], "colour": "green"},
+]
 
 def main():
     win = Window(800,600)
+    for line in lines:
+        point_a = Point(line["point_a"][0], line["point_a"][1])
+        point_b = Point(line["point_b"][0], line["point_b"][1])
+        line_to_draw = Line(point_a=point_a, point_b=point_b)
+        win.draw_line(line=line_to_draw, fill_colour=line["colour"])
+
+
     win.wait_for_close()
 
 if __name__ == "__main__":
--- window.py
+++ window.py
@@ -1,5 +1,22 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point_a: Point, point_b: Point):
+        self.point_a = point_a
+        self.point_b = point_b
+
+    def draw(self, canvas:Canvas, fill_colour:str):
+        x1 = self.point_a.x
+        y1 = self.point_a.y
+        x2 = self.point_b.x
+        y2 = self.point_b.y
+        canvas.create_line(x1, y1, x2, y2, fill=fill_colour, width=2)
+
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
@@ -20,6 +37,14 @@ class Window:
 
     def close(self):
         self.winrun = False
+
+    def draw_line(self, line: Line, fill_colour):
+        line.draw(canvas=self.canvas, fill_colour=fill_colour)
+
+
+
+
+
         
 
 
